b682e2f283ba3e90158598d7b22f0dd5f438aa76
visla-zhupeiran <peiran.zhu@visla.us>
2021-05-26 17:03:53 +0800

    Initial Commit

---
+++ Virtual-camera-sender/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Virtual-camera-sender
+//
+//  Created by VislaNiap on 2021/5/26.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
